884836461897c96affbaed30b3d25eae95caf403
Antonio <selvas.leon029@gmail.com>
2025-10-06 21:57:22 -0600

    first commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Player } from './player/player';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Player
   ],
   imports: [
     BrowserModule,
